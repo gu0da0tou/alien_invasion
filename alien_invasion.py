558aa45c645a99a1df2a944208250bdca5361861
yu <3398498639@qq.com>
2025-06-07 00:40:38 +0800

    2025.06.07学习

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,13 +30,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
-
-        #删除已消失的子弹
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-        #print(len(bullets))此语句用以检验子弹是否删除
-
+        gf.update_bullets(bullets)
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -13,5 +13,7 @@ class Settings():
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 90,90,90
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3 #限制子弹数量为3颗
+
 

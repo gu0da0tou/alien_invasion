9a4c62f87c64b666dd84eeb4fb67b71a9bf87b44
yu <3398498639@qq.com>
2025-06-08 02:21:31 +0800

    create the first row aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,10 +3,12 @@ import pygame
 from pygame.examples.scrap_clipboard import screen
 from pygame.sprite import Group
 
+from alien import Alien
 from settings import Settings
 from  ship import Ship
 import game_functions as gf
 from bullet import Bullet
+
 
 
 
@@ -19,11 +21,14 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #创建一艘飞船
+    #创建一艘飞船、一个用于存储子弹的编组、一个外星人
     ship = Ship(ai_settings,screen)
-    #创建一个用于存储子弹的编组
     bullets = Group()
+    #alien = Alien(ai_settings, screen)
+    aliens = Group()
 
+    #创建外星人群
+    gf.create_fleet(ai_settings, screen, aliens)
 
     #开始游戏的主循环
     while True:
@@ -31,6 +36,6 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
